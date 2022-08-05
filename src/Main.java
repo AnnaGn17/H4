bc7dc8ccc3523cdf3e8837f52401ab16ad83c6bf
homework-2 <an_1704_na@inbox.ru>
2022-08-05 12:07:42 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+//Задание 1
+        int clientOS = 1;
+        if (clientOS == 1) { System.out.println("Скачать приложение для андроид по ссылке");
+        }
+        else { System.out.println( "Скачать приложение для IOS по ссылке");
+        }
 
     }
+    }
 
-}
